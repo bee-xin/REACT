635ef41a55ddcf2efea97424ed70e1aea1434a7c
bee-xin <bijen.ito@gmail.com>
2024-12-20 13:29:09 +0545

    first react commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "./header"
+
+function App() {
+  return (
+ <Header></Header>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+         <h1>My Lovely Website</h1>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Service</li>
+        <li>Contact</li>
+    </ul>
+    </header>
+  )
+}
+
+export default Header
